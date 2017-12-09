7565ef7bfb9d3ea5b6a6c8da199b135a78419f2b
Uğur YILDIZ <yildiz@diginova.com.tr>
2017-12-10 00:54:50 +0300

    user api added

---
+++ controllers/api/UsersController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace kouosl\user\controllers\api;
+
+use kouosl\user\models\User;
+use Yii;
+
+class UsersController extends DefaultController {
+	
+	public $modelClass = 'kouosl\user\models\User';
+
+	public function actions() {
+		$actions = parent::actions ();
+	
+		return $actions;
+	}
+	
+	public function actionView($id){
+
+		$model = User::findOne($id);
+		
+		if(!$model)
+			return ['status' => '404','message' => 'Not Found'];
+
+		return $model;
+	}
+	
+	public function actionIndex(){
+		return User::find()->all();
+	}
+	
+	public function actionCreate(){
+
+		$postParams = yii::$app->request->post();
+		
+		$model = new User();
+	
+		
+		if($model->load($postParams,'') && $model->validate()){
+			if($model->save())
+			    return ['status' => 1];
+			else
+			    return ['status' => 500];
+		}
+		else
+			return ['status' => 100,'message' => 'Parametre Hatası'];
+		
+	}
+	
+	public function actionUpdate($id){
+
+		$postParams = yii::$app->request->post();
+		
+		$model = User::findOne($id);
+
+		if($model = $this->LoadModel($model, $postParams)){
+				if($model->save())
+					return ['status' => 1];
+				else 
+					return ['status' => 101,'message' => $model->errors];
+		}else
+		    return ['status' => 100];
+	}
+	
+	public function actionDelete($id){
+		
+		if(User::findOne($id)->delete())
+			return ['status' => 1];
+		else
+			return ['stauts' => 100];
+	}
+	
+	public function LoadModel($model,$params)
+	{
+		foreach ($params as $key => $value)
+			$model->hasAttribute($key) ? $model->$key = $value : " "; 
+			
+		return $model;
+	}
+}
